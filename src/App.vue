b952a00ba873c6d0b4939e8270b119ba5bd57d0e
soaviniavo <rsoaviniavo@gmail.com>
2025-10-06 13:22:20 +0300

    first step vue js

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,48 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+const count  = ref(0);
+const movieName = ref("");
+const increment = () => {
+  count.value++ ; 
+}
+const decrement = () => {
+  count.value-- ; 
+}
+const delete_movie = (movie) => {
+  movies.value = movies.value.filter(m => m != movie );
+}
+const addMovie = () => {
+  movies.value.push(movieName.value);
+  movieName.value = ""
+}
+
+
+const movies = ref([
+  'Matrix',
+   ' Fantastic ',
+   'Alice'
+]);
+
+</script>
 
 <template>
   <h1>You did it!</h1>
   <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
+     Compteur : {{ count }}
   </p>
+  <p v-if="count >=5" :style="{color: count >= 5 ? 'red' : 'green' }" >Vous avez cliqué 5 fois </p>
+  <button @click="increment">Incrémenter</button>
+  <button @click="decrement">Décrémenter</button>
+  
+  <ul>
+    <li v-for="movie in movies" :key="movie">
+      {{ movie }} <button @click="delete_movie(movie)">supp</button>
+    </li>
+  </ul>
+  <form action="" @submit.prevent="addMovie">
+   <input type="text" placeholder="Nouveau Film" v-model="movieName">
+  <button @click="decrement">Nouveau Film</button>
+  </form>
 </template>
 
 <style scoped></style>
